a0ac26f3e76a28bbaf109210fc7f4bad8c8f27a4
Murat Yürür <admin@muratyurur.com>
2018-11-23 15:54:05 +0300

    Side Menu
    Navigation menü içeriği düzenlendi...

--- site/panel/application/views/includes/aside.php
+++ site/panel/application/views/includes/aside.php
@@ -53,198 +53,102 @@
         <div class="menubar-scroll-inner">
             <ul class="app-menu">
                 <li class="has-submenu">
-                    <a href="javascript:void(0)" class="submenu-toggle">
+                    <a href="<?php base_url(); ?>">
                         <i class="menu-icon zmdi zmdi-view-dashboard zmdi-hc-lg"></i>
-                        <span class="menu-text">Dashboards</span>
-                        <i class="menu-caret zmdi zmdi-hc-sm zmdi-chevron-right"></i>
+                        <span class="menu-text">Kontrol Paneli</span>
                     </a>
-                    <ul class="submenu">
-                        <li><a href="index.html"><span class="menu-text">Dashboard 1</span></a></li>
-                        <li><a href="dashboard.2.html"><span class="menu-text">Dashboard 2</span></a></li>
-                        <li><a href="dashboard.3.html"><span class="menu-text">Dashboard 3</span></a></li>
-                    </ul>
+                </li>
+
+                <li>
+                    <a href="javascript:void(0)" class="submenu-toggle">
+                        <i class="menu-icon zmdi zmdi-settings zmdi-hc-lg"></i>
+                        <span class="menu-text">Ayarlar</span>
+                    </a>
                 </li>
 
                 <li class="has-submenu">
+                    <a href="javascript:void(0)" class="submenu-toggle">
+                        <i class="menu-icon zmdi zmdi-group zmdi-hc-lg"></i>
+                        <span class="menu-text">Galeriler</span>
+                        <i class="menu-caret zmdi zmdi-hc-sm zmdi-chevron-right"></i>
+                    </a>
+                    <ul class="submenu">
+                        <li><a href="buttons.html"><span class="menu-text">Resim Galerileri</span></a></li>
+                        <li><a href="buttons.html"><span class="menu-text">Video Galerileri</span></a></li>
+                        <li><a href="buttons.html"><span class="menu-text">Dosya Galerileri</span></a></li>
+                    </ul>
+                </li>
+
+                <li>
                     <a href="javascript:void(0)" class="submenu-toggle">
                         <i class="menu-icon zmdi zmdi-layers zmdi-hc-lg"></i>
-                        <span class="menu-text">Layouts</span>
-                        <span class="label label-warning menu-label">2</span>
-                        <i class="menu-caret zmdi zmdi-hc-sm zmdi-chevron-right"></i>
+                        <span class="menu-text">Slider</span>
                     </a>
-                    <ul class="submenu">
-                        <li><a href="../default/index.html"><span class="menu-text">Default</span></a></li>
-                        <li><a href="../topbar/index.html"><span class="menu-text">Topbar</span></a></li>
-                    </ul>
                 </li>
 
-                <li class="has-submenu">
+                <li>
+                    <a href="javascript:void(0)" class="submenu-toggle">
+                        <i class="menu-icon zmdi zmdi-flip-to-front zmdi-hc-lg"></i>
+                        <span class="menu-text">Ürünler</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="javascript:void(0)" class="submenu-toggle">
+                        <i class="menu-icon fa fa-newspaper-o"></i>
+                        <span class="menu-text">Haberler</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="javascript:void(0)" class="submenu-toggle">
+                        <i class="menu-icon zmdi zmdi-calendar-note zmdi-hc-lg"></i>
+                        <span class="menu-text">Eğitimler</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="javascript:void(0)" class="submenu-toggle">
+                        <i class="menu-icon zmdi zmdi-case-check zmdi-hc-lg"></i>
+                        <span class="menu-text">Referanslar</span>
+                    </a>
+                </li>
+
+                <li>
                     <a href="javascript:void(0)" class="submenu-toggle">
                         <i class="menu-icon zmdi zmdi-puzzle-piece zmdi-hc-lg"></i>
-                        <span class="menu-text">UI Kit</span>
-                        <i class="menu-caret zmdi zmdi-hc-sm zmdi-chevron-right"></i>
-                    </a>
-                    <ul class="submenu">
-                        <li><a href="buttons.html"><span class="menu-text">Buttons</span></a></li>
-                        <li><a href="alerts.html"><span class="menu-text">Alerts</span></a></li>
-                        <li><a href="panels.html"><span class="menu-text">Panels</span></a></li>
-                        <li><a href="lists.html"><span class="menu-text">Lists</span></a></li>
-                        <li><a href="icons.html"><span class="menu-text">Icons</span></a></li>
-                        <li><a href="media.html"><span class="menu-text">Media</span></a></li>
-                        <li><a href="widgets.html"><span class="menu-text">Widgets</span></a></li>
-                        <li><a href="tabs.html"><span class="menu-text">Tabs &amp; Accordions</span></a></li>
-                        <li><a href="progress.html"><span class="menu-text">Progress</span></a></li>
-                    </ul>
-                </li>
-
-                <li class="has-submenu">
-                    <a href="javascript:void(0)" class="submenu-toggle">
-                        <i class="menu-icon zmdi zmdi-inbox zmdi-hc-lg"></i>
-                        <span class="menu-text">Mail Box</span>
-                        <i class="menu-caret zmdi zmdi-hc-sm zmdi-chevron-right"></i>
-                    </a>
-                    <ul class="submenu">
-                        <li>
-                            <a href="inbox.html">
-                                <span class="menu-text">Inbox</span>
-                                <span class="label label-primary menu-label">12</span>
-                            </a>
-                        </li>
-                        <li><a href="compose.html"><span class="menu-text">Compose</span></a></li>
-                    </ul>
-                </li>
-
-                <li>
-                    <a href="search.web.html">
-                        <i class="menu-icon zmdi zmdi-search zmdi-hc-lg"></i>
-                        <span class="menu-text">Search</span>
-                    </a>
-                </li>
-
-                <li class="has-submenu">
-                    <a href="javascript:void(0)" class="submenu-toggle">
-                        <i class="menu-icon zmdi zmdi-pages zmdi-hc-lg"></i>
-                        <span class="menu-text">Pages</span>
-                        <i class="menu-caret zmdi zmdi-hc-sm zmdi-chevron-right"></i>
-                    </a>
-                    <ul class="submenu">
-                        <li><a href="profile.html"><span class="menu-text">Profile</span></a></li>
-                        <li><a href="price.html"><span class="menu-text">Prices</span></a></li>
-                        <li><a href="invoice.html"><span class="menu-text">Invoice</span></a></li>
-                        <li><a href="gallery.1.html"><span class="menu-text">Gallery</span></a></li>
-                        <li><a href="gallery.2.html"><span class="menu-text">Gallery 2</span></a></li>
-                        <li><a href="support.html"><span class="menu-text">FAQ</span></a></li>
-                        <li class="has-submenu">
-                            <a href="javascript:void(0)" class="submenu-toggle">
-                                <i class="menu-icon zmdi zmdi-plus zmdi-hc-lg"></i>
-                                <span class="menu-text">Misc Pages</span>
-                                <i class="menu-caret zmdi zmdi-hc-sm zmdi-chevron-right"></i>
-                            </a>
-                            <ul class="submenu">
-                                <li><a href="login.html"><span class="menu-text">Login</span></a></li>
-                                <li><a href="signup.html"><span class="menu-text">Sign Up</span></a></li>
-                                <li><a href="password-forget.html"><span class="menu-text">Reset Password</span></a></li>
-                                <li><a href="unlock.html"><span class="menu-text">Unlock Screen</span></a></li>
-                                <li><a href="404.html"><span class="menu-text">404 Error</span></a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-
-                <li class="has-submenu">
-                    <a href="javascript:void(0)" class="submenu-toggle">
-                        <i class="menu-icon zmdi zmdi-check zmdi-hc-lg"></i>
-                        <span class="menu-text">Forms</span>
-                        <i class="menu-caret zmdi zmdi-hc-sm zmdi-chevron-right"></i>
-                    </a>
-                    <ul class="submenu">
-                        <li><a href="form.layouts.html"><span class="menu-text">Form Layouts</span></a></li>
-                        <li><a href="form.elements.html"><span class="menu-text">Form Elements</span></a></li>
-                        <li><a href="form.custom.html"><span class="menu-text">Customized Elements</span></a></li>
-                        <li><a href="form.plugins.html"><span class="menu-text">Form Plugins</span></a></li>
-                        <li><a href="file-upload.html"><span class="menu-text">File Upload</span></a></li>
-                        <li><a href="form.datetime.html"><span class="menu-text">DateTime Pickers</span></a></li>
-                        <li><a href="editors.html"><span class="menu-text">Editors</span></a></li>
-                    </ul>
-                </li>
-
-                <li class="has-submenu">
-                    <a href="javascript:void(0)" class="submenu-toggle">
-                        <i class="menu-icon zmdi zmdi-storage zmdi-hc-lg"></i>
-                        <span class="menu-text">Tables</span>
-                        <i class="menu-caret zmdi zmdi-hc-sm zmdi-chevron-right"></i>
-                    </a>
-                    <ul class="submenu">
-                        <li><a href="tables.basic.html"><span class="menu-text">Basic Tables</span></a></li>
-                        <li><a href="datatables.html"><span class="menu-text">DataTables</span></a></li>
-                    </ul>
-                </li>
-
-                <li class="has-submenu">
-                    <a href="javascript:void(0)" class="submenu-toggle">
-                        <i class="menu-icon zmdi zmdi-chart zmdi-hc-lg"></i>
-                        <span class="menu-text">Charts</span>
-                        <span class="label label-success menu-label">7</span>
-                        <i class="menu-caret zmdi zmdi-hc-sm zmdi-chevron-right"></i>
-                    </a>
-                    <ul class="submenu">
-                        <li><a href="charts.flot.html"><span class="menu-text">Flot Charts</span></a></li>
-                        <li><a href="echarts.bar.html"><span class="menu-text">Bar echarts</span></a></li>
-                        <li><a href="echarts.pie.html"><span class="menu-text">Pie echarts</span></a></li>
-                        <li><a href="echarts.line.html"><span class="menu-text">Line echarts</span></a></li>
-                        <li><a href="echarts.map.html"><span class="menu-text">Map echarts</span></a></li>
-                        <li><a href="echarts.scatter.html"><span class="menu-text">Scatter echarts</span></a></li>
-                        <li><a href="echarts.custom.html"><span class="menu-text">Custom echarts</span></a></li>
-                    </ul>
-                </li>
-
-                <li class="has-submenu">
-                    <a href="javascript:void(0)" class="submenu-toggle">
-                        <i class="menu-icon zmdi zmdi-pin zmdi-hc-lg"></i>
-                        <span class="menu-text">Maps</span>
-                        <i class="menu-caret zmdi zmdi-hc-sm zmdi-chevron-right"></i>
-                    </a>
-                    <ul class="submenu">
-                        <li><a href="map-google.html"><span class="menu-text">Google Maps</span></a></li>
-                        <li><a href="map-vector.html"><span class="menu-text">Vector Maps</span></a></li>
-                    </ul>
-                </li>
-
-                <li class="has-submenu">
-                    <a href="javascript:void(0)" class="submenu-toggle">
-                        <i class="menu-icon zmdi zmdi-apps zmdi-hc-lg"></i>
-                        <span class="menu-text">Apps</span>
-                        <span class="label label-info menu-label">2</span>
-                        <i class="menu-caret zmdi zmdi-hc-sm zmdi-chevron-right"></i>
-                    </a>
-                    <ul class="submenu">
-                        <li><a href="calendar.html"><span class="menu-text">Calendar</span></a></li>
-                        <li><a href="contacts.html"><span class="menu-text">Contacts</span></a></li>
-                    </ul>
-                </li>
-
-                <li class="menu-separator"><hr></li>
-
-                <li>
-                    <a href="documentation.html">
-                        <i class="menu-icon zmdi zmdi-file-text zmdi-hc-lg"></i>
-                        <span class="menu-text">Documentation</span>
+                        <span class="menu-text">Markalar</span>
                     </a>
                 </li>
 
                 <li>
-                    <a href="javascript:void(0)">
-                        <i class="menu-icon zmdi zmdi-settings zmdi-hc-lg"></i>
-                        <span class="menu-text">Settings</span>
+                    <a href="javascript:void(0)" class="submenu-toggle">
+                        <i class="menu-icon fa fa-user-circle-o"></i>
+                        <span class="menu-text">Kullanıcılar</span>
                     </a>
                 </li>
 
                 <li>
-                    <a href="javascript:void(0)">
-                        <i class="menu-icon zmdi zmdi-language-javascript zmdi-hc-lg"></i>
-                        <span class="menu-text">Angular Version</span>
+                    <a href="javascript:void(0)" class="submenu-toggle">
+                        <i class="menu-icon fa fa-users"></i>
+                        <span class="menu-text">Aboneler</span>
                     </a>
                 </li>
+
+                <li>
+                    <a href="javascript:void(0)" class="submenu-toggle">
+                        <i class="menu-icon zmdi zmdi-lamp zmdi-hc-lg"></i>
+                        <span class="menu-text">Popup Hizmeti</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="javascript:void(0)" class="submenu-toggle">
+                        <i class="menu-icon fa fa-home"></i>
+                        <span class="menu-text">Site Ana Sayfası</span>
+                    </a>
+                </li>
+
             </ul><!-- .app-menu -->
         </div><!-- .menubar-scroll-inner -->
     </div><!-- .menubar-scroll -->
