b0966cf5488c0f04439b129ba07ab3473ef92740
Murat Yürür <admin@muratyurur.com>
2018-11-24 01:29:39 +0300

    Basic View Structure and .htaccess
    The ViewFolder structure was set up and .htaccess was activated.

---
+++ site/panel/application/controllers/Dashboard.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+    public $viewFolder = "";
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->viewFolder = "dashboard_v";
+    }
+
+    public function index()
+	{
+		$this->load->view("{$this->viewFolder}/index");
+	}
+}
